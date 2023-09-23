fbefe2c27d48356ce6f77cef4a8a80a0bc57df6e
DAlba-sudo <d.alba.work@gmail.com>
2023-09-23 17:29:03 -0400

    created basic server

--- main.go
+++ main.go
@@ -6,15 +6,20 @@ import (
 	"os"
 )
 
+type GeneralServer struct {
+
+}
+
 func main() {
 
 	// Initial Variable Configuration
 	Port := os.Getenv("PORT")
+	Gs := GeneralServer{}
 
 	// The Mux for the Server
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", Gs.ServeIndex)
 	
-
 	// Driver Code
 	http.ListenAndServe(fmt.Sprintf(":%s", Port), mux)
 }
